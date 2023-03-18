5f553f5eed9dfebf174573834066b3b8b39f151c
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-03-18 16:04:17 -0400

    Updated model path and file drinks.js

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = 4001;
 
+const models = require('./models/Drinks.js');
+const drinks = models.drinks
 
 app.get('/', (req, res) => {
     res.send ('Welcome to the Gitpub App!');
-})
+});
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+});
 
 app.listen(port, () => {
     console.log(`🏝️ Server is listening to PORT ${port} 🎧`)
-})
+});
